41679fef76319cf445ef08b3088ae967808095c4
mac <mac@macdemacbook-pro.local>
2017-12-27 14:51:26 +0800

    Initial Commit

---
+++ Coustom/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Coustom
+//
+//  Created by mac on 2017/12/27.
+//  Copyright © 2017年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Coustom/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Coustom
+//
+//  Created by mac on 2017/12/27.
+//  Copyright © 2017年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
